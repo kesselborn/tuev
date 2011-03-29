02cd5f13ef824ed258f4b4c1927d612d26a9a35a
Daniel Bornkessel <daniel@soundcloud.com>
2011-03-29 15:03:21 +0200

    output nice error message when selenium-server is not running

--- lib/tuev/tuev_runner.rb
+++ lib/tuev/tuev_runner.rb
@@ -6,6 +6,18 @@ class QunitRunner
   end
 
   def run_in_browser(browser_string)
+    begin
+      Net::HTTP.new(@selenium_conf[:host], @selenium_conf[:port]).get2("/")
+    rescue
+      puts 
+      puts "It seems that there is no selenium server listening on '#{@selenium_conf[:host]}:#{@selenium_conf[:port]}'"
+      puts 
+      puts "... aborting (try installing the gem 'selenium-server' and execute 'selenium-server' for an easy solution)"
+      puts 
+      puts 
+      exit(1)
+    end
+
     selenium = Selenium::Client::Driver.new(
       :host => @selenium_conf[:host],
       :port => @selenium_conf[:port],
@@ -28,6 +40,7 @@ class QunitRunner
         browser.open "file://#{@test_file}"
         browser.wait_for_page_to_load "60000"
         puts "\n****** testing #{browser.get_text('css=title')} in #{browser_id}"
+        puts "file:\n\t#{@test_file}\n"
         60.times{ break if (browser.is_element_present("id=qunit-testresult") rescue false); sleep 1 }
         puts browser.get_eval('window.results.join("\n")')
         60.times{ break if (browser.get_text('id=qunit-testresult') != "Running..." rescue false); sleep 1 }
